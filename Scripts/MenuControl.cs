ea174f1882fb3a3d8b80b35ea2c4a25ba63e9fe8
Rosco <rossfarrell7@gmail.com>
2016-08-14 23:24:43 +0800

    Add files via upload

---
+++ Scripts/MenuControl.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
